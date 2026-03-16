4c80968a56ccc41d4e707a407ccfcc0c262c4fbf
kunal2885 <kunalphogat38@gmail.com>
2026-03-16 09:21:18 +0530

    exploring routing and http methods in express

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
 const express = require("express")
 const app = express()
-app.use("/hello",(req,res)=>{
-    res.send("hello from express server")
-})
-app.use("/test",(req,res)=>{
-    res.send("hello from test")
+
+app.get("/user/:userid/:password/:username",(req,res)=>{
+    //console.log(req.query)// to read query parameters
+    console.log(req.params)//to handle and read dynamic routes
+    res.send({
+        "firstname": "kunal",
+        "lastname" : "phogat"
+    })
 })
 
-app.use("/",(req,res)=>{
-    res.send("namaste from home page")
+//advance routing
+app.get("/*fly$/",(req,res)=>{
+    res.send("hello from abc route")
 })
+app.post("/user",(req,res)=>{
+    //creating and saving the user logic
+    res.send("saved the user details successfully")
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted the user successfully")
+})
+app.put("/user",(req,res)=>{
+    res.send("update the user info in db")
+})
+app.use("/test",(req,res)=>{
+    res.send("testing route /test/...")
+})
+
+
+//order of routes matters a lot 
 
 app.listen(8088,()=>{
     console.log("server is up and listening to requests on port 8088")

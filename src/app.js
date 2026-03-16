c3a0d5dab453f76df07e4538811b66bb01a84b26
kunal2885 <kunalphogat38@gmail.com>
2026-03-16 11:07:10 +0530

    explored multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,32 +1,25 @@
 const express = require("express")
 const app = express()
 
-app.get("/user/:userid/:password/:username",(req,res)=>{
-    //console.log(req.query)// to read query parameters
-    console.log(req.params)//to handle and read dynamic routes
-    res.send({
-        "firstname": "kunal",
-        "lastname" : "phogat"
-    })
-})
 
-//advance routing
-app.get("/*fly$/",(req,res)=>{
-    res.send("hello from abc route")
-})
-app.post("/user",(req,res)=>{
-    //creating and saving the user logic
-    res.send("saved the user details successfully")
-})
-app.delete("/user",(req,res)=>{
-    res.send("deleted the user successfully")
-})
-app.put("/user",(req,res)=>{
-    res.send("update the user info in db")
-})
-app.use("/test",(req,res)=>{
-    res.send("testing route /test/...")
-})
+app.use("/user",(req,res,next)=>{
+    console.log("Route handler")
+    //res.send("Response!!!")
+    next()  
+},[(req,res,next)=>{
+    console.log("Route handler 2")
+    //res.send("Response 2")
+    next()
+},(req,res,next)=>{
+    console.log("Route handler 3")
+    //res.send("Response 3")
+    next()
+},
+(req,res,next)=>{
+    console.log("Route handler 4")
+    res.send("Response 4")
+    next()
+}])
 
 
 //order of routes matters a lot 

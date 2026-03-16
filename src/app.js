7176db2da491a94eb1eb92846aecb7eac7bd422a
kunal2885 <kunalphogat38@gmail.com>
2026-03-16 20:15:42 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 const express = require("express")
 const app = express()
-const {adminAuth , userAuth} = require("./midllewares/auth")
-
-app.use("/admin",adminAuth)
-app.post("/user/login",(req,res)=>{
-    res.send("user logged in successfully")
-})
-app.use("/user",userAuth,(req,res)=>{
-    res.send("sent user data")
-})
 
 
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("all data sent")
+app.use("/getUser",(req,res)=>{
+
+    //db call logic
+    try{throw new Error()
+    res.send("sent user data")}
+    catch(err){
+        res.status(500).send("some error")
+
+    }
 })
-app.get("/admin/delete",(req,res)=>{
-    res.send("deleted a user")
+
+//wildcard error handling
+
+app.use("/",(err,req,res,next)=>{
+    res.status(500).send("something went wrong")
 })
+
+
+
 
 
 //order of routes matters a lot 

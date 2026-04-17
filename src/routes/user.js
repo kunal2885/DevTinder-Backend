2b3dae0118363faf2a7e0be2033d555a9bd0a860
kunal2885 <kunalphogat38@gmail.com>
2026-04-17 16:33:07 +0530

    added pagination to feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -48,6 +48,11 @@ userRouter.get("/user/connections",userAuth, async(req,res)=>{
 
 userRouter.get("/user/feed",userAuth, async (req,res)=>{
     try{
+        const page = parseInt(req.query.page) || 1
+        
+        let limit = parseInt(req.query.limit) || 10
+        limit = limit > 50 ? 50 : limit
+        const skip = (page-1)*limit
         const loggedInUser = req.user
         const hideConnections = await ConnectionRequestModel.find({
             $or : [{fromUserId : loggedInUser._id} , {toUserId : loggedInUser._id}]
@@ -57,13 +62,16 @@ userRouter.get("/user/feed",userAuth, async (req,res)=>{
             hideUsersFromFeed.add(req.fromUserId.toString())
             hideUsersFromFeed.add(req.toUserId.toString())
         })
-        console.log(hideUsersFromFeed)
+        
+        
        const users = await User.find({
          $and : [ 
             { _id : {$nin : Array.from(hideUsersFromFeed)}},
             { _id : {$ne : loggedInUser._id} }
          ]
-         }).select(SAFE_USER_DATA);
+         }).select(SAFE_USER_DATA)
+         .skip(skip)
+         .limit(limit);
         res.send(users)
 
 
